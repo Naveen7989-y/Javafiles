d9eb4dc20ebae3aa8dfd0e24d77a12b388860105
Naveen7989-y <yerraguntanaveen7989@gmail.com>
2024-07-21 21:42:55 +0530

    second commit

--- src/palindrome.java
+++ src/palindrome.java
@@ -5,10 +5,15 @@ public class palindrome {
         System.out.println("enter any number");
         int row= sc.nextInt();
         int i,j;
-        for(i=0;i<=row;i++){
-            for(j=i;j>=i;j--){
-               System.out.print(j+"*");
-
+        for(i=1;i<=row;i++){
+            for(j=1;j<=2*(row-i);j++){
+               System.out.print(" ");
+            }
+            for(j=i;j>=1;j--) {
+                System.out.print(j+" ");
+            }
+            for(j=2;j<=i;j++){
+                System.out.print(j+" ");
             }
             System.out.println();
         }
